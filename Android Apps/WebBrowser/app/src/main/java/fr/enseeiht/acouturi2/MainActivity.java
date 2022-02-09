206d09678bb42fdb03bf7c00d90ab5d612b1cbe3
CoutCout <arthur.couturier.2000@gmail.com>
2022-02-09 11:48:04 +0100

    added android web app

---
+++ Android Apps/WebBrowser/app/src/main/java/fr/enseeiht/acouturi2/MainActivity.java
@@ -0,0 +1,35 @@
+package fr.enseeiht.acouturi2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Browser Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button searchButton = (Button) findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.editText);
+                Editable textInputEditText = editText.getEditableText();
+                String url = textInputEditText.toString();
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            }
+        });
+    }
+}
